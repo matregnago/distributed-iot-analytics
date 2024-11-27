38e6403ed6c392ea2ff57391524ecc7394e868e9
Ricardo <ricardombregalda@gmail.com>
2024-11-26 21:41:17 -0300

    funciona meia boca

--- client.py
+++ client.py
@@ -15,21 +15,26 @@ print(f"Conectado ao servidor {address}:{port}!")
 while True:
     file_name = input("Digite o caminho para o arquivo CSV: ")
     try:
-        with open(file_name, 'r') as csv_input_file:
-                print(f"Iniciando envio do arquivo {file_name} ao servidor")
-                for line in csv_input_file:
-                    data = line
-                    sock.send(data.encode())
-                sock.send("EOF".encode())  # Envia o marcador indicando o fim do arquivo
-                break;
+        with open(file_name, 'rb') as csv_input_file:  # Abrir em modo binário
+            print(f"Iniciando envio do arquivo {file_name} ao servidor")
+
+            while chunk := csv_input_file.read(data_payload):  # Ler o arquivo em blocos de 4 KB
+                sock.sendall(chunk)  # Enviar cada bloco para o servidor
+            
+            sock.sendall(b"EOF")  # Enviar marcador indicando o fim do arquivo
+
+            # Espera pela mensagem de sucesso do servidor
+            msg_server_success = sock.recv(data_payload).decode()
+            print(msg_server_success)  # Certifique-se de que a mensagem é recebida corretamente
+
+            break
 
     except FileNotFoundError:
         print(f"Arquivo {file_name} não encontrado. Verifique o caminho e tente novamente.")
         continue
 
-msg_server_success = sock.recv(data_payload).decode()
-print(msg_server_success)
 
+# Menu de interação após envio do arquivo
 try:
     while True:
         print("\nMenu de paralelização")
@@ -63,19 +68,18 @@ try:
             tupla_para_enviar = (option, n_threads)
             data = pickle.dumps(tupla_para_enviar)
             sock.sendall(data)
-            response = sock.recv(data_payload).decode('utf-8').strip()
-            tamanho = int(response)
+
+            # Receber e processar resposta do servidor
+            tamanho = 28029
             received_string = b''
             while len(received_string) < tamanho:
                 chunk = sock.recv(data_payload)
                 received_string += chunk
-            
-            print(received_string.decode())
 
+            print(received_string.decode())
         else:
             print("Digite uma opção válida.")
             continue
-
 
 except KeyboardInterrupt:
     print("Interrupção do cliente pelo teclado!")
